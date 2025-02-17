96197686c6c5c85e5471e76d4bbb6cd691c8fef0
NurlanAmanov <nurlanemenov14@gmail.com>
2025-02-17 22:10:36 +0400

    sdsfsdf

--- src/Context/SettingContex.jsx
+++ src/Context/SettingContex.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { DeleteHeadbanner, GetHeadderbanner, UpdateHeadbanner } from '../service/Setting';
+import { AddHeadbanner, DeleteHeadbanner, GetHeadderbanner, UpdateHeadbanner } from '../service/Setting';
 
 
 export const SETTIGDATA = createContext([]);
